d43ca917fe2e2dc0d07d1378c2fa4b6550abdbfb
Danilo Souza Alexandre <danilo.alexandre@sistemapoliedro.com.br>
2019-03-12 17:52:43 -0300

    apparently installed element

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div 
+    class="todo-item" 
+    :class="{'is-complete':todo.completed}"
+    :is_complete="{'true':todo.completed}">
+        <p>
+            <input type="checkbox" v-model="todo.completed"/>
+            {{todo.title}}
+            <button class="del">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ['todo']
+}
+</script>
+
+<style scoped>
+    .todo-item{
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete{
+        text-decoration: line-through;
+    }
+
+    .del{
+        background: rgb(196, 59, 59);
+        color: white;
+        float: right;
+        border-radius: 50%;
+        width: 22px;
+        height: 22px;
+        font-size: 14px;
+        font-weight: 300;
+    }
+</style>
+
---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div :key="todo.id" v-for="todo in list">
+            <h3>
+                <todo-item v-bind:todo="todo" />
+            </h3>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name: 'Todos',
+    components:{
+        TodoItem
+    },
+    props: ['list']
+}
+</script>
+
+
+<style scoped>
+
+</style>
